ccbc7e16710219b8682833d92db85c0c0eb8ab67
Jason House <jh5975@gmail.com>
2025-09-09 22:08:36 -0400

    Adding GitHub Actions

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ const customJestConfig = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
+      branches: 75,
       functions: 80,
       lines: 80,
       statements: 80,
@@ -30,6 +30,9 @@ const customJestConfig = {
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
+    '<rootDir>/e2e/',
+    '<rootDir>/playwright-report/',
+    '<rootDir>/test-results/',
   ],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   globals: {
