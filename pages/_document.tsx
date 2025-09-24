aa32ea323c4cd2528a9fcb1ae935db24675fe1de
Jason House <jh5975@gmail.com>
2025-09-24 16:25:17 -0400

    Removing Plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,20 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          defer
-          data-domain="jasonhhouse.github.io"
-          src="https://plausible.io/js/script.js"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.plausible = window.plausible || function () {
-                (window.plausible.q = window.plausible.q || []).push(arguments);
-              };
-            `,
-          }}
-        />
       </Head>
       <body>
         <Main />
