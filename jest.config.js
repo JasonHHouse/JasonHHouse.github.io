a86385013480b1e413970aec328c4511b3e27703
Jason House <jh5975@gmail.com>
2025-09-30 10:21:29 -0400

    Updating jest coverage to 70

--- jest.config.js
+++ jest.config.js
@@ -22,9 +22,9 @@ const customJestConfig = {
   coverageThreshold: {
     global: {
       branches: 70,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
   },
   testPathIgnorePatterns: [
